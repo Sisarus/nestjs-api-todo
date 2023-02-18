8234fca932e3fd185cc3da1600c1fbb4b97892ae
Alisa <alisa.luoma@hotmail.fi>
2023-02-18 18:03:16 +0200

    Changed sql data better for assigment

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from "@nestjs/common/decorators";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService{
   constructor(private prisma: PrismaService){}
 
   signup(dto: AuthDto){
+    //generate the password hash
+
+    const hash = argon.hash(dto.password);
+    
+
+    //save the new user in the db
+
+    //return the saved user
     return 'signup ka';
   }
 
