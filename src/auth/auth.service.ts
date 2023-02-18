60fc024191308c94c6bd4fb483839be92aa8a87d
Alisa <alisa.luoma@hotmail.fi>
2023-02-18 17:56:48 +0200

    Adding class-validator and class-transformer

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from "@nestjs/common/decorators";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
 
 @Injectable()
 export class AuthService{
   constructor(private prisma: PrismaService){}
-  signin(){
-    return 'signup';
+
+  signup(dto: AuthDto){
+    return 'signup ka';
   }
 
-  signup(){
-    return 'signup ka';
+  signin(dto: AuthDto){
+    return 'signup';
   }
 }
